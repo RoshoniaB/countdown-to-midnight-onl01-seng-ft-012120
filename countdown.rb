f55d9f1c8b3cdaf08dd66e1c71abc4b51d56c753
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-01-30 08:58:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 def countdown(seconds_to_midnight) 
   while seconds_to_midnight > 0
     puts "#{seconds_to_midnight} SECOND(S)!"
-    countdown -= 1
+    seconds_to_midnight -= 1
   end
+  
 end
 
 def countdown_with_sleep = 5
